60c01e2958a0319f583bec42d7007daf8df9b95b
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2018-01-24 22:17:12 +0200

    Hello World

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Configuration
+
+# Directory where backups are kept
+export BACKUP_DIRECTORY="${HOME}/backups"
+
+# Directory where releases are kept
+export RELEASES_DIRECTORY="${HOME}/releases"
